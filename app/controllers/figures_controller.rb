efd0b487660dbcff42fd04f09047b7b911fb6db7
Erik Warringer <erikwarringer@gmail.com>
2017-12-15 02:04:13 -0500

    add figures show path

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,4 +2,8 @@ class FiguresController < ApplicationController
   def index
     @figures = Figure.all
   end
+
+  def show
+    @figure = Figure.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'figures#index'
   resources :posts, only: [:index, :create]
-  resources :figures, only: [:index, :create]
+  resources :figures, only: [:index, :show, :create]
 end
